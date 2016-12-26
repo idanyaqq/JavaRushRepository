dc6090f1f246938df37dde8eaf18a9fc655bc410
Danya <danucemo994@gmail.com>
2016-12-26 16:29:41 +0300

    new lessons

--- test/level25/lesson09/task01/Solution.java
+++ test/level25/lesson09/task01/Solution.java
@@ -7,8 +7,26 @@ package com.javarush.test.level25.lesson09.task01;
 3. Если это Throwable, то вывести в консоль "ХЗ"
 Реализуйте эту логику.
 */
-public class Solution extends Thread {
-    public Solution() {
-        if
+public class Solution extends Thread
+{
+    public Solution()
+    {
+        setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler()
+        {
+            @Override
+            public void uncaughtException(Thread t, Throwable e)
+            {
+                if (e instanceof Error)
+                {
+                    System.out.println("Нельзя дальше работать");
+                } else if (e instanceof Exception)
+                {
+                    System.out.println("Надо обработать");
+                } else if (e instanceof Throwable)
+                {
+                    System.out.println("ХЗ");
+                }
+            }
+        });
     }
 }
